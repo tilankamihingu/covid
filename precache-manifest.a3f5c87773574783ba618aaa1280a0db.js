d0474b431858898f6cd956d10e0f0cdb38bf12ae
tilankamihingu <tilankamihingu2018@gmail.com>
2020-06-26 10:04:23 -0700

    Updates

---
+++ precache-manifest.a3f5c87773574783ba618aaa1280a0db.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "327606b2f011908c8f2303eb3df4bdf1",
+    "url": "/covidnew/index.html"
+  },
+  {
+    "revision": "47464d6f0e3c89024a36",
+    "url": "/covidnew/static/css/main.2731a5ac.chunk.css"
+  },
+  {
+    "revision": "9b15ca3aa5b32813930f",
+    "url": "/covidnew/static/js/2.b0fd06b8.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covidnew/static/js/2.b0fd06b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47464d6f0e3c89024a36",
+    "url": "/covidnew/static/js/main.13b84339.chunk.js"
+  },
+  {
+    "revision": "d9a38a9695edc5b017b0",
+    "url": "/covidnew/static/js/runtime-main.2aac0167.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covidnew/static/media/image.d7265326.png"
+  }
+]);
